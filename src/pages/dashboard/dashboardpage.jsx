b82b31a84df47838d49d9ba8423f4e9d586470eb
maaquin <lucianomaquin@gmail.com>
2025-08-19 22:27:19 -0600

    se agrega el proyecto

--- src/pages/dashboard/dashboardpage.jsx
+++ src/pages/dashboard/dashboardpage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
-import { Content } from "../../components/dashboard/Content";
+import { Content } from "../../components/Dashboard/Content";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 import {
